101e8eea0d9ab1b7691f33dafe8e2f7b3ff566a7
micah-gould <Gouldmicah@gmail.com>
2024-02-20 18:35:55 +0100

    Added a basic test case

---
+++ testCases.js
@@ -0,0 +1,40 @@
+// <![CDATA[
+import {
+  addExercise,
+  Graph /* and others as needed */
+} from '../codecheck-tracer/script/codecheck_tracer.js' // Update to correct filepath
+addExercise(function * (sim, state) {
+  // TODO Eliminate
+  if (state === undefined) {
+    state = { seed: sim.randSeed() }
+  } else {
+    sim.randSeed(state.seed)
+  }
+  // Add vertices
+  const g = new Graph()
+  const n = 6
+  for (let i = 0; i < n; i++) g.vertex()
+  g.verts().forEach((v) => {
+    v.d = v === g.verts()[0] ? 0 : '∞'
+    v.π = ''
+  })
+
+  // Add edges
+  for (let i = 0; i < n - 1; i++) g.edge(g.verts()[i], g.verts()[i + 1])
+  let target = 5
+  while (target > 0) {
+    const [a, b] = sim.randDistinctInts(2, 0, n - 1).sort((a, b) => a - b)
+    const v = g.verts()[a]
+    const w = g.verts()[b]
+    if (g.findEdge(v, w) === undefined) {
+      g.edge(v, w)
+      target--
+    }
+  }
+
+  g.layout(sim, 1, 0.5, 15, 15)
+
+  yield sim.askAll(item => { item.color = 'pink' }, g.verts())
+  yield sim.askAll(item => { item.color = 'pink' }, g.edges())
+})
+// ]]>
